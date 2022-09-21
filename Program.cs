3cb686342ed8dc7068902a181d19176bfa99c3d9
nikita <fireshape360@inbox.ru>
2022-09-21 10:12:52 +0300

    Added input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длинна которых <= 3 символа. Первоначальный массив можно 
+ввести с клавиатуры или задать со старта*/
+
+Console.WriteLine("Задайте размер массива в виде числа");
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int m);
+if (isNumber == false)
+{
+    Console.WriteLine("Неверные данные");
+    return;
+}
+
+
+string[] array = new string[m];
+
+string[] secondArray = new string[m];
+
